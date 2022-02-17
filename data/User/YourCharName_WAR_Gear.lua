10cca9aade23fb8eddfda2cfd4fede200df5348a
KateFFXI <dual.avcs@gmail.com>
2022-02-16 19:33:18 -0500

    Added example folder for character
    Follow this syntax to install GS for your character.

---
+++ data/User/YourCharName_WAR_Gear.lua
@@ -0,0 +1,13 @@
+function user_setup()
+
+	include('Common/WAR_UserSetup_Common.lua')
+
+end
+
+function init_gear_sets()
+
+	include('Common/WAR_Common.lua')
+	
+end
+
+include('Common/WAR_Functions_Common.lua')
