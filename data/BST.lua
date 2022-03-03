441d7f1b8d349d95c0c51f53c5f9e5b869342565
KateFFXI <dual.avcs@gmail.com>
2022-03-03 18:05:29 -0500

    Merged BST lua
    Deletion of BLU BST DRK RUN SAM functions

--- data/BST.lua
+++ data/BST.lua
@@ -255,6 +255,14 @@ end
 -------------------------------------------------------------------------------------------------------------------
 -- Set eventArgs.handled to true if we don't want any automatic gear equipping to be done.
 -- Set eventArgs.useMidcastGear to true if we want midcast gear equipped on precast.
+function select_default_macro_book()
+	-- Default macro set/book
+	if player.sub_job == 'WAR' then
+		set_macro_page(1, 9)
+	else
+		set_macro_page(1, 9)
+	end
+end
 
 function job_filtered_action(spell, eventArgs)
 
@@ -667,26 +675,33 @@ function check_pet()
 end
 
 function check_ready()
-	if state.AutoReadyMode.value and not moving then
+	if state.AutoReadyMode.value and player.in_combat then
+	local abil_recasts = windower.ffxi.get_ability_recasts()
+	
 		if pet.isvalid then
-			if pet.status == "Engaged" and get_current_ready_count() > 0 then
-				windower.send_command('gs c ready')
-				tickdelay = os.clock() + 2
-				return true
-			elseif pet.status == "Idle" and player.target.type == "MONSTER" then
-				windower.chat.input('/pet Fight <t>')
-				tickdelay = os.clock() + 2
+			if get_current_ready_count() > 0 and abil_recasts[102] < latency  then
+				if pet.status == "Idle" and player.target.type == "MONSTER" and abil_recasts[100] < latency then
+					windower.chat.input('/pet Fight <t>')
+					tickdelay = os.clock() + 2
+					return true
+				elseif pet.status == "Engaged" then 
+					windower.send_command('gs c ready')
+					tickdelay = os.clock() + 2
+					return true
+				end
+			elseif get_current_ready_count() <= 0 and abil_recasts[102] > latency and pet.status == "Engaged" and not buffactive['Unleash'] then
+				windower.send_command:schedule(1,'input /pet "Heel" <me>')
+				tickdelay = os.clock() + 3
 				return true
 			else
 				return false
-			end
+			end	
 		else
 			return false
 		end
 	else
 		return false
 	end
-
 end
 
 function get_current_ready_count()
