d3f7cbbb29354f81abf0613e6577e96bf06a87de
PeachBlossomWine <52470877+PeachBlossomWine@users.noreply.github.com>
2024-12-17 21:44:59 -0500

    Fix healbot reloads

--- data/PLD.lua
+++ data/PLD.lua
@@ -727,10 +727,11 @@ end
 
 function check_zerg_sp()
 	if state.AutoZergMode.value == 'On' and player.in_combat and not data.areas.cities:contains(world.area) then
-		add_to_chat(123, 'Zerg Mode ACTIVATION')
+		
 		local abil_recasts = windower.ffxi.get_ability_recasts()
 
 		if (abil_recasts[254] < latency) then
+			add_to_chat(123, 'Zerg Mode ACTIVATION')
             windower.chat.input('/ja "Intervene" <t>')
 			tickdelay = os.clock() + 1.8
 			return true		
