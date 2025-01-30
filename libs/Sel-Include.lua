4b095f4ef8750d6139f8885d39ea6c93da9c5a43
PeachBlossomWine <52470877+PeachBlossomWine@users.noreply.github.com>
2025-01-30 12:17:45 -0500

    Allow movement speed on all idle sets
    Don't block moving equipment on random idle sets, we gonna kite we gonna kite

--- libs/Sel-Include.lua
+++ libs/Sel-Include.lua
@@ -2115,7 +2115,8 @@ end
 -- Function to add kiting gear on top of the base set if kiting state is true.
 -- @param baseSet : The gear set that the kiting gear will be applied on top of.
 function apply_kiting(baseSet)
-	if sets.Kiting and (state.Kiting.value or (player.status == 'Idle' and moving and state.DefenseMode.value == 'None' and state.Passive.value == 'None' and (state.IdleMode.value == 'Normal' or state.IdleMode.value:contains('Sphere') or state.IdleMode.value == 'DT' or state.IdleMode.value == 'Refresh'))) then --or not (player.in_combat or being_attacked)))) then
+	--and (state.IdleMode.value == 'Normal' or state.IdleMode.value:contains('Sphere') or state.IdleMode.value == 'DT' or state.IdleMode.value == 'Refresh')
+	if sets.Kiting and (state.Kiting.value or (player.status == 'Idle' and moving and state.DefenseMode.value == 'None' and state.Passive.value == 'None')) then
 		baseSet = set_combine(baseSet, sets.Kiting)
 	end
 	
